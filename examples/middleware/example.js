04a85d06568c9724b10cdc4e0bcd6a797073663a
jonschlinkert <dev@sellside.com>
2014-05-05 07:13:58 -0400

    adds htmlmin to example

--- examples/middleware/example.js
+++ examples/middleware/example.js
@@ -1,15 +1,23 @@
 module.exports = function(assemble) {
   var options = assemble.config;
-  // console.log(assemble)
+
+
+  /**
+   * The actual plugin
+   */
 
   var middleware = function (params, next) {
-    console.log('assemble-middleware-foo', params.event);
     // do stuff
     next();
   };
 
 
-  // Define a single `event`, or an `events` array
+
+  /**
+   * Plugin events, define a single `event`
+   * or an `events` array
+   */
+
   middleware.event = 'page:before:render';
   return {
     'assemble-middleware-foo': middleware
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var verb = require('gulp-verb');
 var assemble = require('./');
 
 /**
@@ -8,7 +10,15 @@ var assemble = require('./');
 gulp.task('assemble', function () {
   gulp.src('test/fixtures/pages/*.hbs')
     .pipe(assemble({assemblerc: '.assemblerc.yml'}))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('_gh_pages/'));
 });
 
-gulp.task('default', ['assemble']);
+
+gulp.task('verb', function () {
+  gulp.src(['.verbrc.md'])
+    .pipe(verb({dest: 'README.md'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['assemble', 'verb']);
